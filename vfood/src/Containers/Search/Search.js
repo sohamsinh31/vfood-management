daa88baf882a8219269ba4cfad4da5929b84aafa
sohamsinh31 <sohamb2019@gmail.com>
2023-07-23 21:33:49 +0530

    search component added

---
+++ vfood/src/Containers/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+const Search = () => {
+    const [searchParams,setSearchParams] = useSearchParams();
+
+    const searchTerm = searchParams.get('q') || '';
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Search
