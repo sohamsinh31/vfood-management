1db3551e990da08cdb6bfc5491a1d6eefc6a3c2b
sohamsinh31 <sohamb2019@gmail.com>
2024-01-08 02:53:19 +0530

    minor changes

--- vfood/src/Services/Constant.js
+++ vfood/src/Services/Constant.js
@@ -1,7 +1,8 @@
 // const url = "http://192.168.0.112:"
-const url = "https://vfood-sohamsinh31.vercel.app/"
-// const port = ":5000"
-const port = ""
+const url = "http://localhost"
+// const url = "https://vfood-sohamsinh31.vercel.app/"
+const port = ":5050"
+// const port = ""
 const Token = "8VHhm78KqG"
 
 export { url, port, Token }
