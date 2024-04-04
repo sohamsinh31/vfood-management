f9f45898f0c069c1930fdd4c5f8e34533ff71de5
sohamsinh31 <sohamb2019@gmali.com>
2024-04-05 01:35:03 +0530

    minor changes

--- vfood/src/Components/NavBar/NavBar.js
+++ vfood/src/Components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./styles.css"
 import logo from "../../Assets/vfood.png"
+import mb from "../../Assets/menubutton.png"
 
 const NavBar = () => {  
   return (
     <div className='mainNavbar'>
       <img src={logo} alt='logo' className='logoImage' />
       <input type='text' className='navInput' placeholder='SEARCH' />
-      <img src="../../Assets/menubutton.png" alt='Menu' className='menuImage' />
+      <img src={mb} alt='Menu' className='menuImage' />
     </div>
   )
 }
